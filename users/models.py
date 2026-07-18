9d1b38e307bda1483527f1204a8c5c69b4fd3c28
orozof216-ux <orozof216@gmail.com>
2026-07-18 08:39:39 +0600

    homework8

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
 from users.managers import CustomUserManager
 
 
@@ -44,3 +45,20 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class ConfirmationCode(models.Model):
+    user = models.OneToOneField(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name="confirmation_code"
+    )
+
+    code = models.CharField(max_length=6)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.code
